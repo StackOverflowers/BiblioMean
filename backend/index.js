9d475e302146ea882e9fa3d54e03e89f182539c4
Stiven Posada Valcárcel <sf.posada21@gmail.com>
2021-08-24 13:29:32 -0500

    1:30 pm

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,11 @@ const app = express();
 const Role = require("../backend/routes/role");
 app.use(express.json());
 app.use(cors());
-<<<<<<< HEAD
 
 app.use("/api/user", User);
 
 app.use('/api/role',Role);
 app.use("/api/library" ,Library);
-=======
-app.use('/api/role',Role);
-app.use("/api/library" ,Library);
-
->>>>>>> dev
 
 app.listen(process.env.PORT, () =>{
     console.log("Backend server running OK, on port: ",process.env.PORT );
