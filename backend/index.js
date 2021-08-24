2dc6ab1a3e1b4c77273601535c805e478a17f763
Anthony Ahumada <locopala99@gmail.com>
2021-08-24 09:40:37 +0200

    more js

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,12 @@ const app = express();
 const Role = require("../backend/routes/role");
 app.use(express.json());
 app.use(cors());
-<<<<<<< HEAD
+
 
 app.use('/api/role',Role);
-
-
-
-=======
 app.use("/api/library" ,Library);
->>>>>>> ab5ffd42e101d2201921c5922c64f0727a98a6a4
+
+
 
 app.listen(process.env.PORT, () =>{
     console.log("Backend server running OK, on port: ",process.env.PORT );
---
+++ backend/models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const bookSchema = new mongoose.Schema({
+    name:String,
+    author:String,
+    library:{ type: mongoose.Schema.ObjectId, ref: "library" },
+    date:{type:Date,default:Date.now},
+    Status: Boolean,
+});
+
+const book = mongoose.model('book', bookSchema);
+
+
+module.exports = book;
