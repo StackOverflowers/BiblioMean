429d71db646cfd4036a8252b6b991d42ae42f4b3
Camilo Rodriguez <camilo.rod2412@gmail.com>
2021-08-24 13:39:32 -0500

    Middleware Auth and validateUser

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("./db/db");
 const Library = require("./routes/library"); 
+const User = require("./routes/user");
+const Role = require("../backend/routes/role");
 require("dotenv").config();
 
-const User = require("./routes/user");
-
-
 const app = express();
-const Role = require("../backend/routes/role");
+
 app.use(express.json());
 app.use(cors());
-
 app.use("/api/user", User);
-
 app.use('/api/role',Role);
 app.use("/api/library" ,Library);
 
